b8af83d5d1e4ea8312784b9e8cfd47186658a03e
rimpal <thakorrimpal30@gmail.com>
2025-09-18 00:27:54 +0530

    add vercel deploy link in server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ dotenv.config();
 const app = express();
 
 // Middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://forfrontend-mzzt.vercel.app", credentials: true }));
 app.use(express.json());
 
 // Routes
